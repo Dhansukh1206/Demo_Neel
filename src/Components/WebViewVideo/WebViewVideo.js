e64609dd856e45287552d7bafba487706464d2b0
Neel Patel <neelpatel.6531@gmail.com>
2022-09-19 10:57:37 +0530

    init web viewr

---
+++ src/Components/WebViewVideo/WebViewVideo.js
@@ -0,0 +1,28 @@
+import React, { useEffect, useRef } from "react";
+import WebViewer from '@pdftron/webviewer';
+import { initializeVideoViewer } from '@pdftron/webviewer-video';
+
+export function WebViewVideo() {
+  const viewer = useRef(null);
+
+  useEffect(() => {
+    WebViewer(
+      {
+        path: "/webviewer/lib",
+        selectAnnotationOnCreation: true,
+      },
+      viewer.current
+    ).then(async (instance) => {
+      const license =
+        "---- Insert commercial license key here after purchase ----";
+
+      const { loadVideo } = await initializeVideoViewer(instance, { license });
+
+      const videoUrl =
+        "https://pdftron.s3.amazonaws.com/downloads/pl/video/video.mp4";
+      loadVideo(videoUrl);
+    });
+  }, []);
+
+  return <div className="webviewer" ref={viewer}></div>;
+}
---
+++ src/Components/WebViewVideo/index.js
@@ -0,0 +1,1 @@
+export { WebViewVideo } from "./WebViewVideo";
